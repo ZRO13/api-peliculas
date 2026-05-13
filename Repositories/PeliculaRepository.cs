a3f2dd14e93405e99a39a6083689feee900d990e
Zero13Dev <juliocoloma1875@gmail.com>
2026-05-13 09:59:49 -0500

    tarea api peliculas.

--- Repositories/PeliculaRepository.cs
+++ Repositories/PeliculaRepository.cs
@@ -4,31 +4,55 @@ namespace ApiPeliculas.Repositories
 {
     public class PeliculaRepository : IPeliculaRepository
     {
-        private static List<Pelicula> peliculas = new List<Pelicula>
-        {
-            new Pelicula
+        private static List<Pelicula> peliculas = new List<Pelicula>{
+          new Pelicula
             {
                 Id = 1,
-                Titulo = "Interstellar",
-                Director = "Christopher Nolan",
+                Titulo = "El Código Enigma",
+                Director = "Morten Tyldum",
                 Disponible = true
             },
 
             new Pelicula
             {
                 Id = 2,
-                Titulo = "Titanic",
-                Director = "James Cameron",
+                Titulo = "Ex Machina",
+                Director = "Alex Garland",
                 Disponible = true
             },
 
             new Pelicula
             {
                 Id = 3,
-                Titulo = "Avatar",
-                Director = "James Cameron",
+                Titulo = "Inteligencia Artificial",
+                Director = "Steven Spielberg",
+                Disponible = false
+            },
+
+            new Pelicula
+            {
+                Id = 4,
+                Titulo = "Her",
+                Director = "Spike Jonze",
+                Disponible = true
+            },
+
+            new Pelicula
+            {
+                Id = 5,
+                Titulo = "The Social Network",
+                Director = "David Fincher",
+                Disponible = true
+            },
+
+            new Pelicula
+            {
+                Id = 6,
+                Titulo = "Minority Report",
+                Director = "Steven Spielberg",
                 Disponible = false
             }
+        
         };
 
         public List<Pelicula> ObtenerTodas()
